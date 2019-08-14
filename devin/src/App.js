265c35307032608f71ecbd81a2aae1e5d5b89762
devin-bielejec <devin.bieleje@gmail.com>
2019-08-14 17:51:39 -0400

    Set up users state

--- devin/src/App.js
+++ devin/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import FormikForm from "./Form";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
   return (
     <div className="App">
       <header className="App-header">
